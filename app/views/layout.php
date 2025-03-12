fa96ebc290f7f6299f49e751459ce47f93a921b1
DzmitryiKorjik <dmardovitch@gmail.com>
2025-03-12 15:53:38 +0100

    modifie SEO

--- app/views/layout.php
+++ app/views/layout.php
@@ -1,11 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Beercraft</title>
-    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/style.css?v=<?php echo time(); ?>">
-</head>
+    <?php require_once 'app/views/partials/head.php'; ?>
 <body>
     <?php require_once 'app/views/partials/header.php'; ?>
     
--- app/views/partials/head.php
+++ app/views/partials/head.php
@@ -1,6 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Boutique de bières artisanales avec la meilleure sélection de bières locales et importées.">
     <title>Beercraft</title>
     <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/style.css?v=<?php echo time(); ?>">
 </head>
